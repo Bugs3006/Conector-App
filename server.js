bced869e540f00cdcbca379e35d6cfd16eea27f2
Sumeet Anand <sumeetanand.ind@classichome.com>
2025-06-11 18:06:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,3 @@ const server = createServer((req, res) => {
 });
  
 // starts a simple http server locally on port 3000
-server.listen(8081, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:8081');
-});
