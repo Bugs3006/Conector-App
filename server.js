b70cb34a785341b21869bc3a8681e9715505daa7
Sumeet Anand <sumeetanand.ind@classichome.com>
2025-06-11 18:43:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-import { createServer } from 'node:http';
+// import { createServer } from 'node:http';
  
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello hello World!\n');
+// const server = createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello hello World!\n');
+// });
+ 
+import express from 'express';
+const app = express();
+
+// Serve files in 'public' folder at root URL
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
- 
-// starts a simple http server locally on port 3000
