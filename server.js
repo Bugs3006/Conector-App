c570a7183b50ebff0bb110b6cca7a5b6916e8ece
sumeet <sumeetanand.ind@classichome.com>
2025-06-12 22:53:52 +0530

    fix npw

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.all(
 );
 
 // Start Express server
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Listening on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
