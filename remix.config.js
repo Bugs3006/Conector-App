091c05f92528792c6990bdbc4ce0765f6b4d8218
Sumeet Anand <sumeetanand.ind@classichome.com>
2025-06-12 15:51:31 +0530

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
-// Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
-// stop passing in HOST, so we can remove this workaround after the next major release.
+// Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server.
+// The CLI will eventually stop passing in HOST, so we can remove this workaround after the next major release.
 if (
   process.env.HOST &&
   (!process.env.SHOPIFY_APP_URL ||
@@ -14,15 +14,10 @@ if (
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
+  server: "./server.js", // Entry point to your custom server
   serverModuleFormat: "cjs",
-  dev: { port: process.env.HMR_SERVER_PORT || 8002 },
+  dev: {
+    port: process.env.HMR_SERVER_PORT || 8002,
+  },
   future: {},
-};
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
-module.exports = {
-  server: "./server.js", // or your Remix server entry
-  // ...
-  // No need to change other config
 };
