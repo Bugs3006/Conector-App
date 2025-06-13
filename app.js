195810ae542aa42890327ab2f02bef7dba693157
sumeet <sumeetanand.ind@classichome.com>
2025-06-13 15:14:35 +0530

    fix npuy

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // web/index.js
 import app from './app.js';
- 
-app.listen(process.env.PORT || 3000, '0.0.0.0');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- server.js
+++ server.js
@@ -144,4 +144,10 @@ app.all(
 );
 
 // Start Express server
-app.listen(process.env.PORT || 3000, '0.0.0.0');
+import app from './app.js';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
